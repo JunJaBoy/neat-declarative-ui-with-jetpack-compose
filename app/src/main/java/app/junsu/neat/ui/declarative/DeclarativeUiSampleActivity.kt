fbe8fb45de7efe91ed8219969ed5bdfbf170f773
junsuPark <krpjs0508@gmail.com>
2024-08-07 20:14:45 +0900

    theme

--- app/src/main/java/app/junsu/neat/ui/declarative/DeclarativeUiSampleActivity.kt
+++ app/src/main/java/app/junsu/neat/ui/declarative/DeclarativeUiSampleActivity.kt
@@ -3,12 +3,15 @@ package app.junsu.neat.ui.declarative
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import app.junsu.neat.ui.theme.NeatDeclarativeUIWithJetpackComposeTheme
 
 class DeclarativeUiSampleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            NeatDeclarativeUIWithJetpackComposeTheme {
             DeclarativeUiSampleContent()
+                }
         }
     }
 }
--- app/src/main/java/app/junsu/neat/ui/music/MusicUiSampleActivity.kt
+++ app/src/main/java/app/junsu/neat/ui/music/MusicUiSampleActivity.kt
@@ -3,12 +3,15 @@ package app.junsu.neat.ui.music
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import app.junsu.neat.ui.theme.NeatDeclarativeUIWithJetpackComposeTheme
 
 class MusicUiSampleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MusicUiSampleContent()
+            NeatDeclarativeUIWithJetpackComposeTheme {
+                MusicUiSampleContent()
+            }
         }
     }
 }
